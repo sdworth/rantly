277e0638daeaae21028eeb8404e2728cf092a9c0
Savannah Worth <7469614+sdworth@users.noreply.github.com>
2014-08-20 14:50:56 -0600

    users can register

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,5 +4,20 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:notice] = 'You have registered successfully!'
+      redirect_to root_path
+    else
+      flash[:notice] = 'Please fill out all fields!'
+      render :new
+    end
+  end
 
+  private
+
+  def user_params
+    params[:user].permit(:username, :password, :first_name, :last_name, :bio, :frequency)
+  end
 end
--- spec/features/root_spec.rb
+++ spec/features/root_spec.rb
@@ -8,9 +8,16 @@ feature 'homepage' do
     expect(page).to have_content('Jeff Taggart, avid Ranter')
   end
 
-  scenario 'can view registration page' do
+  scenario 'can register' do
     visit '/'
     click_button 'JOIN'
-    fill_in 'Username', with: 'monochrome'
+    fill_in 'user_username', with: 'monochrome'
+    fill_in 'user_password', with: 'schnee'
+    fill_in 'user_first_name', with: 'Blake'
+    fill_in 'user_last_name', with: 'Belladona'
+    fill_in 'user_bio', with: 'former member of the White Fang'
+    choose 'user_frequency_daily'
+    click_button 'REGISTER'
+    expect(page).to have_content 'You have registered successfully!'
   end
 end
