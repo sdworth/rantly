74249580cf8be886cdc1756848d66b43fd4faf17
Savannah Worth <7469614+sdworth@users.noreply.github.com>
2014-10-09 12:40:21 -0600

    profile links work

--- spec/features/profile_spec.rb
+++ spec/features/profile_spec.rb
@@ -5,11 +5,21 @@ feature 'profile' do
   before :each do
     @user = create_user
     @other_user = create_other_user
-    create_rant(@other_user)
+    @rant = create_rant(@other_user)
     login(@user)
   end
 
   scenario 'can edit profile' do
+    click_on @other_user.first_name
+
+    expect(page).to have_content full_name(@other_user)
+    expect(page).to have_link Follow
+    expect(page).to have_content @other_user.bio
+  end
+
+  scenario 'can access profile from rant show' do
+    click_on @rant.rant
+
     click_on @other_user.first_name
 
     expect(page).to have_content full_name(@other_user)
