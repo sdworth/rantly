3d4f89f6717bf3e6bb09f9497b43ebe9e85dfd8a
Savannah Worth <7469614+sdworth@users.noreply.github.com>
2014-10-18 21:43:01 -0600

    can search based on rant text/title

--- app/models/search.rb
+++ app/models/search.rb
@@ -5,9 +5,10 @@ class Search
     @search_text = text
   end
 
-  def get_rants_based_on_user
-    response = User.search search_text
+  def get_rants
+    user_based = User.search search_text
+    rant_based = Rant.search search_text
 
-    Rant.where(user_id: response.records.records)
+    Rant.where(user_id: user_based.records.records) + rant_based.records.records
   end
 end
